a0285490c279f239dceb0b64155ce0a6beae4e69
Matt Neary <neary@kensho.com>
2015-12-07 01:09:15 -0500

    implement basic information collection from tmux

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,17 @@ module Main where
 
 import System.Tmux
 
-main =
+parseInt x = read x :: Int
+
+workspaceWindows :: IO [Int]
+workspaceWindows =
   do windows <- listWindows (Target "workspace")
-     putStrLn $ show windows
+     return $ map (parseInt . snd . head) windows
+
+nextWorkspaceWindow :: IO Int
+nextWorkspaceWindow =
+  do windows <- workspaceWindows
+     return $ (maximum windows) + 1
+
+main = nextWorkspaceWindow >>= (putStrLn . show)
 
